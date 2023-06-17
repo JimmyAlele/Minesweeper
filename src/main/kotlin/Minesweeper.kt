5a7937c1db1555594df32fa94919acba077fb38c
JimmyAlele <135094151+JimmyAlele@users.noreply.github.com>
2023-06-17 17:16:13 +0300

    minesweeper update

--- src/main/kotlin/Minesweeper.kt
+++ src/main/kotlin/Minesweeper.kt
@@ -1,9 +1,22 @@
+package minesweeper
+import kotlin.random.Random
+const val ROWS = 9
+const val COLUMNS = 9
+
 fun main() {
-    val a: Int = readln().toInt()
-    val b: Int = readln().toInt()
-    var i = 0
-    for (num in a .. b) {
-        i += num
+    println("How many mines do you want on the field?")
+    val numberOfMines = readln().toInt()
+    val rowsWithMines = mutableListOf<Int>()
+    repeat(numberOfMines) {
+        rowsWithMines.add(Random.nextInt(1, ROWS + 1))
     }
-    println(i)
 }
+
+    /* generate the mines using random numbers
+    var rowPosition = Int
+    var columnPosition: Int
+    for (mine in 1..numberOfMines) {
+        rowPosition = Random.nextInt(0, ROWS)
+        columnPosition = Random.nextInt(0, COLUMNS)
+    }*/
+
