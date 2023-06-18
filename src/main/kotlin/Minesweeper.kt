ca03f161fc5f04d7a1131da4ec86db23f3fc9ce4
JimmyAlele <135094151+JimmyAlele@users.noreply.github.com>
2023-06-18 05:43:08 +0300

    minesweeper updated with comments

--- src/main/kotlin/Minesweeper.kt
+++ src/main/kotlin/Minesweeper.kt
@@ -10,16 +10,19 @@ fun main() {
     val rowsWithMines = mutableListOf<Int>()
     val numberOfMinesPerRow: MutableList<Int> = mutableListOf<Int>()
 
+    // randomly generate list of rows to hold mines
     repeat(numberOfMines) {
         rowsWithMines.add(Random.nextInt(1, ROWS + 1))
     }
 
+    // calculate total number of mines on each row as generated
     for (row in 1..ROWS) {
         numberOfMinesPerRow.add(rowsWithMines.count { it == row })
     }
 
     // each row can only have as many mines as its columns
     // check that none of the rows have more mines than they can accommodate
+    // generate the number of mines on each row again if necessary
     while (numberOfMinesPerRow.maxOrNull()!! > COLUMNS) {
         rowsWithMines.clear()
         numberOfMinesPerRow.clear()
@@ -31,9 +34,8 @@ fun main() {
         for (row in 1..ROWS) {
             numberOfMinesPerRow.add(rowsWithMines.count { it == row })
         }
-        println(numberOfMinesPerRow)
     }
-
+    // print the rows
     for (row in 1..ROWS) {
         if (row in rowsWithMines) {
             val predicate: (Int) -> Boolean = { it == row }
@@ -45,10 +47,11 @@ fun main() {
                 println(rowTemplateCopy.joinToString(""))
             } else {
                 val rowTemplateCopy = rowTemplate.toMutableList()
+                // randomly place the mines on the rows
+                // avoid placing more than one mine on the same spot
                 while (rowTemplateCopy.count { it == "X" } < numberOfMinesInRow) {
                     rowTemplateCopy[Random.nextInt(0, COLUMNS)] = "X"
                 }
-
                 println(rowTemplateCopy.joinToString(""))
             }
         } else {
