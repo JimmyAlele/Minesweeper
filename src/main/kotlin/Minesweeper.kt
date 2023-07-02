20b8acf47a7ad6d647f2ab005e5167b3cac00e24
JimmyAlele <135094151+JimmyAlele@users.noreply.github.com>
2023-07-02 06:46:43 +0300

    minesweeper updated to include mines and hints to mine locations

--- src/main/kotlin/Minesweeper.kt
+++ src/main/kotlin/Minesweeper.kt
@@ -11,39 +11,46 @@ fun main() {
     val mineField1 = MineField(ROWS, COLUMNS, numberOfMines)
     mineField1.printMineField(mineField1.displayedField)
     val mineLocations = mineField1.addMines()
-    mineField1.addHints()
-    //mineField1.printMineField(mineField1.field)
-    mineField1.printMineField(mineField1.displayedField)
     println(mineLocations)
     mineField1.takeGuesses(mineLocations)
 }
 
 class MineField (private val rows: Int, private val columns: Int, private val numberOfMines: Int) {
-    val field = MutableList(rows) {MutableList(columns) {"."} }
+    private val field = MutableList(rows) {MutableList(columns) {"."} }
     val displayedField = MutableList(rows) {MutableList(columns) {"."} }
 
     fun addMines (): MutableList<MutableList<Int>> {
         println("Set/unset mine marks or claim a cell as free:")
-        val (y, x) = readln().split(" ").subList(0, 2).map {it.toInt() - 1}
+        val (freeY, freeX) = readln().split(" ").subList(0, 2).map {it.toInt() - 1}
         val mineLocations = mutableListOf<MutableList<Int>>()
-        mineLocations.add(mutableListOf(x, y))
-        field[x][y] = "X"
-        var n: Int = 1 //starting with the first free mine location entered by the user
+        var n = 0
 
         while (n != numberOfMines) {
             val x: Int = Random.nextInt(0, rows)
             val y: Int = Random.nextInt(0, columns)
 
-            if (field[x][y] != "X") {
+            if (field[x][y] != "X" && (x != freeX && y != freeY)) {
                 field[x][y] = "X"
                 mineLocations.add(mutableListOf(x, y))
                 n++
             }
+
+            for (i in maxOf(freeX - 1, 0) .. minOf(freeX + 1, ROWS - 1)) {
+                for (j in maxOf(0, freeY - 1) .. minOf(freeY + 1, COLUMNS - 1)) {
+                    if (field[i][j] == "X") {
+                        field[i][j] = "."
+                        n -= 1
+                    }
+                }
+            }
         }
+        addHints()
+        floodFill(freeX,freeY)
+        printMineField(displayedField)
         return mineLocations
     }
 
-    fun addHints () {
+    private fun addHints () {
         for (row in 0 until ROWS) {
             for (column in 0 until COLUMNS) {
                 if (field[row][column] != "X") {
@@ -87,7 +94,9 @@ class MineField (private val rows: Int, private val columns: Int, private val nu
                 "free" -> {
                     if (mutableListOf(x, y) in locations) {
                         println("You stepped on a mine and failed!")
+                        for (loc in locations) { displayedField[loc[0]][loc[1]] = "X" }
                         printMineField(displayedField)
+                        break
                     } else {
                         floodFill(x, y)
                         printMineField(displayedField)
@@ -101,7 +110,7 @@ class MineField (private val rows: Int, private val columns: Int, private val nu
         println("Congratulations! You found all the mines!")
     }
 
-    fun printMineField (sampleField: MutableList<MutableList<String>>): Unit {
+    fun printMineField (sampleField: MutableList<MutableList<String>>) {
         val printedField = sampleField.map { it. toMutableList() }.toMutableList()
         printedField.addAll(0, MutableList(1) {MutableList(sampleField[0].size) {"-"} })
         printedField.addAll(printedField.lastIndex + 1, MutableList(1) {MutableList(sampleField[0].size) {"-"} })
@@ -127,23 +136,25 @@ class MineField (private val rows: Int, private val columns: Int, private val nu
     }
 
     private fun floodFill (x: Int, y: Int) {
-        val queue = mutableListOf<MutableList<Int>>(mutableListOf(x, y))
+        var count = 0
+        val queue = mutableListOf(mutableListOf(x, y))
         do {
             for (loc in queue) {
                 val a = loc[0]
                 val b = loc[1]
                 displayedField[a][b] = "/"
+                count ++
                 queue.remove(mutableListOf(a, b))
-                for (x in maxOf(a - 1, 0) .. minOf(a + 1, ROWS - 1)) {
-                    for (y in maxOf(0, b - 1) .. minOf(b + 1, COLUMNS - 1)) {
-                        when (field[x][y]) {
-                            "." -> queue.add(mutableListOf(x, y))
-                            "1", "2", "3", "4", "5", "6", "7", "8" -> displayedField[x][y] = field[x][y]
+                for (i in maxOf(a - 1, 0) .. minOf(a + 1, ROWS - 1)) {
+                    for (j in maxOf(0, b - 1) .. minOf(b + 1, COLUMNS - 1)) {
+                        when (field[i][j]) {
+                            "." -> queue.add(mutableListOf(i, j))
+                            "1", "2", "3", "4", "5", "6", "7", "8" -> displayedField[i][j] = field[i][j]
                         }
                     }
                 }
             }
-        } while (queue.isNotEmpty())
+        } while (queue.size != count)
     }
 
 
